832ddf4523f28a4f6d37b59a9972b069950a91b9
Brandon <brandonkanute@gmail.com>
2022-04-06 12:32:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2 ;
+
+const random = Math.floor(Math.random()* 100)
+
+let num3 = 24;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1,2,3,4,20);
